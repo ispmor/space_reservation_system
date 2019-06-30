a40f645b89628d474da4010b3501acc4851c1504
jbryl7 <37078470+jbryl7@users.noreply.github.com>
2019-06-30 19:53:23 +0200

    deleted login and register button when logged in

--- dr/urls.py
+++ dr/urls.py
@@ -13,11 +13,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('email/', views.email, name='email'),
     path('forgot_password/', auth_views.PasswordResetView.as_view(template_name='forgot_password.html'), name='forgot_password'),
-    path('forgot_password/done/', auth_views.PasswordResetDoneView.as_view(template_name='forgot_password_sent.html'), name='forgot_password_done'),
-    path('forgot_password/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='forgot_password_confirm'),
+    path('forgot_password/done/', auth_views.PasswordResetDoneView.as_view(template_name='forgot_password_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='forgot_password_confirm'),
     path('forgot_password/complete/', auth_views.PasswordResetCompleteView.as_view(), name='forgot_password_complete'),
     path('reservation/', views.reservation, name='reservation'),
     path('failed_reservation/', views.failed_reservation, name='failed_reservation'),
     path('failed_register/', views.failed_register, name='failed_register')
-
 ]
