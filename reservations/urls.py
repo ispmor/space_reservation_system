194e8023950811cb846905cee7626bf60c69b5c2
puszkarb <puszkarski.bartosz@gmail.com>
2019-09-04 10:24:52 +0100

    update, read button are now functional

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,7 +4,7 @@ from .views import ReservationUpdateView, ReservationCreateView, ReservationDele
 from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
 
 urlpatterns = [
-    path('read/<uidb64>', ReservationReadView.as_view(), name='read_reservation'),
+    path('read/<uuid:id>', ReservationReadView.as_view(), name='read_reservation'),
     path('update/<uidb64>', ReservationUpdateView.as_view(), name='update_reservation'),
     path('create/', ReservationCreateView.as_view(), name='create_reservation'),
     path('delete/<uidb64>', ReservationDeleteView.as_view(), name='delete_reservation'),
--- reservations/views.py
+++ reservations/views.py
@@ -14,12 +14,14 @@ from bootstrap_modal_forms.generic import (BSModalLoginView,
                                            BSModalReadView,
                                            BSModalDeleteView)
 
+
 # Create your views here.
 class ReservationCreateView(BSModalCreateView):
     template_name = 'reservation/modals/create.html'
     form_class = ReservationForm
     success_message = 'Success: Reservation was created.'
     success_url = reverse_lazy('home')
+
 
 class ReservationUpdateView(BSModalUpdateView):
     model = Reservation
@@ -28,10 +30,16 @@ class ReservationUpdateView(BSModalUpdateView):
     success_message = 'Success: Reservation was updated.'
     success_url = reverse_lazy('home')
 
+
 class ReservationReadView(BSModalReadView):
     model = Reservation
     template_name = 'reservation/modals/read.html'
     success_url = reverse_lazy('home')
+
+    def get(self, request, id):
+        context = {"reservation": Reservation.objects.filter(id=id)}
+        return render(request, self.template_name, context)
+
 
 class ReservationDeleteView(BSModalDeleteView):
     model = Reservation
@@ -39,16 +47,19 @@ class ReservationDeleteView(BSModalDeleteView):
     success_message = 'Success: Reservation was deleted.'
     success_url = reverse_lazy('home')
 
+
 class ReservationListView(generic.ListView):
     def get(self, request):
         template_name = 'reservation/list.html'
         now = timezone.now()
         if (request.user.is_authenticated):
             user = request.user
-            reservations = Reservation.objects.filter(user=CustomUser.objects.get(username=user.username).id, archived = False, end_reservation__gte = now - timedelta(hours = 1))
+            reservations = Reservation.objects.filter(user=CustomUser.objects.get(username=user.username).id,
+                                                      archived=False, end_reservation__gte=now - timedelta(hours=1))
             context = {"reservations": reservations}
             return render(request, template_name, context)
         return render(request, template_name)
+
 
 def reservation(request):
     if request.user.is_authenticated:
@@ -64,35 +75,36 @@ def reservation(request):
                 reservation.start_reservation = form.cleaned_data.get('start_reservation')
                 reservation.end_reservation = form.cleaned_data.get('end_reservation')
                 reservation.description = form.cleaned_data.get('description')
-                if not validate_reservation_date(reservation) :
+                if not validate_reservation_date(reservation):
                     return HttpResponseRedirect('reservations')
                 reservation.save()
                 return HttpResponseRedirect('/')
-            else :
+            else:
                 return HttpResponseRedirect('reservations')
-        else :
+        else:
             form = ReservationForm()
-            return render(request, 'reservation/reservation.html',{'form': form})
+            return render(request, 'reservation/reservation.html', {'form': form})
     else:
         return HttpResponseRedirect('')
 
+
 def validate_reservation_date(reservation):
     reservation_list = Reservation.objects.filter(room=reservation.room, status='a').order_by("-start_reservation")
-    
-    if 0 < len(reservation_list) <= 25 :
-        number_of_checked_reservations = len(reservation_list) -1
-    elif len(reservation_list) > 0 :
+
+    if 0 < len(reservation_list) <= 25:
+        number_of_checked_reservations = len(reservation_list) - 1
+    elif len(reservation_list) > 0:
         number_of_checked_reservations = 25
     else:
         number_of_checked_reservations = 0
     for r in reservation_list[: number_of_checked_reservations]:
         print("=======", r.start_reservation, reservation.start_reservation)
-        print("+++++++", r.end_reservation, reservation.end_reservation )
-        if r.start_reservation < reservation.start_reservation < r.end_reservation or r.start_reservation < reservation.end_reservation < r.end_reservation :
+        print("+++++++", r.end_reservation, reservation.end_reservation)
+        if r.start_reservation < reservation.start_reservation < r.end_reservation or r.start_reservation < reservation.end_reservation < r.end_reservation:
             return False
     return True
 
-#legacy:
+# legacy:
 # def failed_reservation(request):
 #     if request.user.is_authenticated:
 #         return render(request, 'failed_reservation.html')
@@ -105,7 +117,7 @@ def validate_reservation_date(reservation):
 #     else:
 #         return render(request, 'failed_register.html')
 
-        
+
 # def reservations(request):
 #     now = timezone.now()
 #     if request.user.is_authenticated:
